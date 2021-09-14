2347aff1560dc32ae51df78e5830f1416b58fb25
JonathanGzzBen <jonathangzzben@gmail.com>
2021-09-14 07:47:10 -0500

    Add terminal options example

---
+++ terminaloptions/src/main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <ostream>
+
+#include "../libs/cxxopts/include/cxxopts.hpp"
+
+int main(int argc, char** argv) {
+  cxxopts::Options options("TerminalOptions", "Just a test to parse options");
+
+  options.add_options()("b,bar", "Param bar", cxxopts::value<std::string>())(
+      "d,debug", "Enable debugging",
+      cxxopts::value<bool>()->default_value("false"))(
+      "f,foo", "Param foo", cxxopts::value<int>()->default_value("10"))(
+      "h,help", "Print usage");
+
+  auto result = options.parse(argc, argv);
+
+  if (result.count("help")) {
+    std::cout << options.help() << std::endl;
+    exit(0);
+  }
+  bool debug = result["debug"].as<bool>();
+  if (debug) {
+    std::cout << "running in debug" << std::endl;
+  }
+  std::string bar;
+  if (result.count("bar")) {
+    bar = result["bar"].as<std::string>();
+    std::cout << "bar: " << bar << std::endl;
+  }
+  int foo = result["foo"].as<int>();
+  if (foo > 0) {
+    for (int i{0}; i < foo; i++) {
+      std::cout << i << std::endl;
+    }
+  }
+
+  return 0;
+}
