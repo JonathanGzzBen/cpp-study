dab5ab07d60c4be19199b8bf5cd566b8bef7f669
JonathanGzzBen <jonathangzzben@gmail.com>
2021-09-20 09:06:59 -0500

    Refactor tboxes menu

--- tboxes/src/main.cpp
+++ tboxes/src/main.cpp
@@ -6,6 +6,10 @@
 
 std::vector<ToyBox*> toyboxes;
 void print_menu();
+void add_box(std::vector<ToyBox*>& toyboxes);
+void list_toyboxes(const std::vector<ToyBox*>& toyboxes);
+void display_toybox(const std::vector<ToyBox*>& toyboxes);
+void add_triangle(std::vector<ToyBox*>& toyboxes);
 
 int main(int argc, char** argv) {
   char input{'\0'};
@@ -16,37 +20,21 @@ int main(int argc, char** argv) {
     switch (input) {
       case 'c':
       case 'C': {
-        std::cout << "Enter box name: ";
-        std::string box_name;
-        std::cin >> box_name;
-        ToyBox* new_box = new ToyBox{box_name};
-        std::cout << "Created ToyBox " << new_box->get_name() << std::endl;
-        toyboxes.push_back(new_box);
+        add_box(toyboxes);
         break;
       }
       case 'l':
       case 'L': {
-        std::cout << "Boxes:" << std::endl;
+        list_toyboxes(toyboxes);
         break;
       }
       case 'd':
       case 'D': {
-        std::cout << "Which box do you want to display?: ";
-        std::string box_name;
-        std::cin >> box_name;
-        std::cout << "Displaying contents of box " << box_name << std::endl;
+        display_toybox(toyboxes);
       }
       case 't':
       case 'T': {
-        std::cout << "Enter measures of new triangle" << std::endl;
-        std::cout << "Triangle base: ";
-        int triangle_base{0};
-        std::cin >> triangle_base;
-        std::cout << "Triangle height: ";
-        int triangle_height{0};
-        std::cin >> triangle_height;
-        auto* triangle = new Triangle{triangle_base, triangle_height};
-        std::cout << *triangle << std::endl;
+        add_triangle(toyboxes);
       }
     }
     std::cout << std::endl << std::endl;
@@ -65,4 +53,36 @@ void print_menu() {
   std::cout << "d  - Display ToyBox contents" << std::endl;
   std::cout << "at - Add Triangle to ToyBox" << std::endl;
   std::cout << "q  - Quit" << std::endl << std::endl;
+}
+
+void add_box(std::vector<ToyBox*> &toyboxes) {
+  std::cout << "Enter box name: ";
+  std::string box_name;
+  std::cin >> box_name;
+  ToyBox* new_box = new ToyBox{box_name};
+  std::cout << "Created ToyBox " << new_box->get_name() << std::endl;
+  toyboxes.push_back(new_box);
+}
+
+void list_toyboxes(const std::vector<ToyBox*>& toyboxes) {
+  std::cout << "Boxes:" << std::endl;
+}
+
+void display_toybox(const std::vector<ToyBox*>& toyboxes) {
+  std::cout << "Which box do you want to display?: ";
+  std::string box_name;
+  std::cin >> box_name;
+  std::cout << "Displaying contents of box " << box_name << std::endl;
+}
+
+void add_triangle(std::vector<ToyBox*>& toyboxes) {
+  std::cout << "Enter measures of new triangle" << std::endl;
+  std::cout << "Triangle base: ";
+  int triangle_base{0};
+  std::cin >> triangle_base;
+  std::cout << "Triangle height: ";
+  int triangle_height{0};
+  std::cin >> triangle_height;
+  auto* triangle = new Triangle{triangle_base, triangle_height};
+  std::cout << *triangle << std::endl;
 }
