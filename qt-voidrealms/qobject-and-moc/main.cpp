9c5a4b84684afbad0e58a4354a9f1f87b4f95f21
JonathanGzzBen <jonathangzzben@gmail.com>
2021-12-30 18:43:41 -0600

    Add qobject-and-moc tutorial code

---
+++ qt-voidrealms/qobject-and-moc/main.cpp
@@ -0,0 +1,19 @@
+#include "test.h"
+#include <QCoreApplication>
+#include <QDebug>
+
+int main(int argc, char *argv[]) {
+  QCoreApplication a(argc, argv);
+
+  Test test;
+
+  // Connect the signal and the slot
+  QObject::connect(&test, &Test::close, &a, &QCoreApplication::quit,
+                   Qt::QueuedConnection);
+
+  test.dostuff();
+
+  int value = a.exec();
+  qInfo() << "Exit value:" << value;
+  return value;
+}
