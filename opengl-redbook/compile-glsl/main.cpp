9ccc16fd83b0edb2dfc9c71add80aed04b6d135d
JonathanGzzBen <jonathangzzben@gmail.com>
2022-09-25 15:41:38 -0500

    Add shaders reading

--- opengl-redbook/compile-glsl/main.cpp
+++ opengl-redbook/compile-glsl/main.cpp
@@ -1,47 +1,96 @@
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
 
+std::string load_source_file(const std::string& filename) {
+  std::ifstream input_file{filename};
+  if(!input_file.is_open()) {
+    std::cerr << "Error opening file " << filename << "\n";
+    exit(EXIT_FAILURE);
+  }
+
+  std::string file_content;
+  while (input_file.good()) {
+    std::string buf;
+    input_file >> buf;
+    file_content += buf + "\n";
+  }
+
+  return file_content;
+}
+
 int main() {
-    GLFWwindow* window;
+  /* Initialize the library */
+  if (!glfwInit()) return -1;
 
-    /* Initialize the library */
-    if (!glfwInit())
-      return -1;
-
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-      glfwTerminate();
-      return -1;
-    }
-
-    /* Make the window's context current */
-    glfwMakeContextCurrent(window);
-
-    GLenum err = glewInit();
-    if (GLEW_OK != err)
-    {
-      /* Problem: glewInit failed, something is seriously wrong. */
-      std::cerr << "Error: " << glewGetErrorString(err) << "\n";
-      exit(EXIT_FAILURE);
-    }
-
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-      /* Render here */
-      glClear(GL_COLOR_BUFFER_BIT);
-
-      /* Swap front and back buffers */
-      glfwSwapBuffers(window);
-
-      /* Poll for and process events */
-      glfwPollEvents();
-    }
-
+  /* Create a windowed mode window and its OpenGL context */
+  GLFWwindow* window =
+      glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
+  if (!window) {
     glfwTerminate();
-    return 0;
+    return -1;
+  }
+
+  /* Make the window's context current */
+  glfwMakeContextCurrent(window);
+
+  GLenum err = glewInit();
+  if (GLEW_OK != err) {
+    /* Problem: glewInit failed, something is seriously wrong. */
+    std::cerr << "Error: " << glewGetErrorString(err) << "\n";
+    exit(EXIT_FAILURE);
+  }
+
+  const auto shader = glCreateShader(GL_VERTEX_SHADER);
+  std::string shader_source_str{load_source_file("shaders/hello.glsl")};
+
+  const char* shader_src = shader_source_str.c_str();
+  glShaderSource(shader, 1, &shader_src, nullptr);
+  glCompileShader(shader);
+  int compile_status;
+  glGetShaderiv(shader, GL_COMPILE_STATUS, &compile_status);
+  if (compile_status != GL_TRUE) {
+    int log_length;
+    glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &log_length);
+    std::string log;
+    log.resize(log_length);
+    glGetShaderInfoLog(shader, GL_INFO_LOG_LENGTH, &log_length, log.data());
+    std::cout << log;
+    exit(EXIT_FAILURE);
+  }
+
+
+  /* Create program */
+  auto program = glCreateProgram();
+  glAttachShader(program, shader);
+  glLinkProgram(program);
+  int link_status;
+  glGetProgramiv(program, GL_LINK_STATUS, &link_status);
+  if (link_status != GL_TRUE) {
+    int log_length;
+    glGetProgramiv(program, GL_INFO_LOG_LENGTH, &log_length);
+    std::string log;
+    log.resize(log_length);
+    glGetProgramInfoLog(program, GL_INFO_LOG_LENGTH, &log_length, log.data());
+    std::cout << log;
+    exit(EXIT_FAILURE);
+  }
+
+  /* Loop until the user closes the window */
+  while (!glfwWindowShouldClose(window)) {
+    /* Render here */
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    /* Swap front and back buffers */
+    glfwSwapBuffers(window);
+
+    /* Poll for and process events */
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
 }
