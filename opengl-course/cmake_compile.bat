6be310628d30fdb12bd47ea601095fb2f27208c9
JonathanGzzBen <jonathangzzben@gmail.com>
2023-01-25 12:09:31 -0600

    Update CMakeLists.txt in opengl-course to use FetchContent

--- opengl-course/cmake_compile.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-C:/msys64/msys2_shell.cmd -defterm -here -no-start -clang64 -c "compile_cmake_project.sh && cd build && ./main.exe"
--- opengl-course/cmake_run.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-C:/msys64/msys2_shell.cmd -defterm -here -no-start -clang64 -c "cd build && ./main.exe"
