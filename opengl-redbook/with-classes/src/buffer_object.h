97acef8358df167412851b88a5d93ada84beec04
JonathanGzzBen <jonathangzzben@gmail.com>
2023-01-29 18:26:40 -0600

    Refactor with class for buffer objects

---
+++ opengl-redbook/with-classes/src/buffer_object.h
@@ -0,0 +1,19 @@
+#ifndef BUFFER_OBJECT_H
+#define BUFFER_OBJECT_H
+#include "GL/glew.h"
+
+class BufferObject {
+ private:
+  unsigned int buffer_name;
+
+ public:
+  BufferObject(void* data, size_t size, int flags = 0) {
+    glCreateBuffers(1, &buffer_name);
+    glNamedBufferStorage(buffer_name, size, data, flags);
+  }
+  ~BufferObject() = default;
+
+  auto GetBufferName() -> unsigned int { return buffer_name; }
+};
+
+#endif  // BUFFER_OBJECT_H
